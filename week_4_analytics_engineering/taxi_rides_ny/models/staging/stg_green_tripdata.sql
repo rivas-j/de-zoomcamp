0f0ac7f2d9be2ea53224d5c0776bacbbe8cb56db
Jonathan Rivas <jrivas404@gmail.com>
2023-02-19 15:23:25 -0500

    updated wk 4 staging schema for dbt - green trip data

--- week_4_analytics_engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ week_4_analytics_engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -4,7 +4,7 @@ with tripdata as
 (
   select *,
     row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
-  from {{ source('staging','green_tripdata') }}
+  from {{ source('staging','external_green_tripdata') }}
   where vendorid is not null 
 )
 select
