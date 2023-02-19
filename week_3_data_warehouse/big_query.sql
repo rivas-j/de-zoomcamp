736785ceb520511f40ba01028b7d2768dbefd056
Jonathan Rivas <jrivas404@gmail.com>
2023-02-19 14:56:18 -0500

    updated week 3 bigquery sql query. updated wk 4 dbt schema

--- week_3_data_warehouse/big_query.sql
+++ week_3_data_warehouse/big_query.sql
@@ -5,58 +5,74 @@ LIMIT 100;
 
 
 -- Creating external table referring to gcs path
-CREATE OR REPLACE EXTERNAL TABLE `taxi-rides-ny.nytaxi.external_yellow_tripdata`
+-- Customizing query table name to match my database
+
+--Yellow Trips
+CREATE OR REPLACE EXTERNAL TABLE `calcium-petal-376805.ny_taxi_test.external_yellow_tripdata`
 OPTIONS (
   format = 'CSV',
-  uris = ['gs://nyc-tl-data/trip data/yellow_tripdata_2019-*.csv', 'gs://nyc-tl-data/trip data/yellow_tripdata_2020-*.csv']
+  uris = ['gs://ny_taxi_prefect/trip_data/yellow_tripdata_2019-*.csv', 'gs://ny_taxi_prefect/trip_data/yellow_tripdata_2020-*.csv']
 );
 
--- Check yello trip data
-SELECT * FROM taxi-rides-ny.nytaxi.external_yellow_tripdata limit 10;
+--Green Trips
+-- DDL: Only accepts CSV files in format under options, but can parse CSV from .gz files as long as format states CSV
+CREATE OR REPLACE EXTERNAL TABLE `calcium-petal-376805.ny_taxi_test.external_green_tripdata`
+OPTIONS (
+  format = 'CSV',
+  uris = ['gs://ny_taxi_prefect/trip_data/green_tripdata_2019-*.gz', 'gs://ny_taxi_prefect/trip_data/green_tripdata_2020-*.gz']
+);
 
--- Create a non partitioned table from external table
-CREATE OR REPLACE TABLE taxi-rides-ny.nytaxi.yellow_tripdata_non_partitoned AS
-SELECT * FROM taxi-rides-ny.nytaxi.external_yellow_tripdata;
+--gs://ny_taxi_prefect/trip_data/yellow_tripdata_2019-02.csv
 
+-- Check yellow trip data
+SELECT * FROM calcium-petal-376805.ny_taxi_test.external_yellow_tripdata limit 10;
+
+-- Create a non partitioned table from external table:Yellow Trips
+CREATE OR REPLACE TABLE calcium-petal-376805.ny_taxi_test.yellow_tripdata_non_partitioned AS
+SELECT * FROM calcium-petal-376805.ny_taxi_test.external_yellow_tripdata;
+
+-- Create a non partitioned table from external table:Green Trips
+CREATE OR REPLACE TABLE calcium-petal-376805.ny_taxi_test.green_tripdata_non_partitioned AS
+SELECT * FROM calcium-petal-376805.ny_taxi_test.external_green_tripdata;
 
 -- Create a partitioned table from external table
-CREATE OR REPLACE TABLE taxi-rides-ny.nytaxi.yellow_tripdata_partitoned
+CREATE OR REPLACE TABLE calcium-petal-376805.ny_taxi_test.yellow_tripdata_partitioned
 PARTITION BY
   DATE(tpep_pickup_datetime) AS
-SELECT * FROM taxi-rides-ny.nytaxi.external_yellow_tripdata;
+SELECT * FROM calcium-petal-376805.ny_taxi_test.external_yellow_tripdata;
 
 -- Impact of partition
 -- Scanning 1.6GB of data
 SELECT DISTINCT(VendorID)
-FROM taxi-rides-ny.nytaxi.yellow_tripdata_non_partitoned
+FROM calcium-petal-376805.ny_taxi_test.yellow_tripdata_non_partitioned
 WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2019-06-30';
 
 -- Scanning ~106 MB of DATA
 SELECT DISTINCT(VendorID)
-FROM taxi-rides-ny.nytaxi.yellow_tripdata_partitoned
+FROM calcium-petal-376805.ny_taxi_test.yellow_tripdata_partitioned
 WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2019-06-30';
 
 -- Let's look into the partitons
 SELECT table_name, partition_id, total_rows
-FROM `nytaxi.INFORMATION_SCHEMA.PARTITIONS`
-WHERE table_name = 'yellow_tripdata_partitoned'
+FROM `ny_taxi_test.INFORMATION_SCHEMA.PARTITIONS`
+WHERE table_name = 'yellow_tripdata_partitioned'
 ORDER BY total_rows DESC;
 
 -- Creating a partition and cluster table
-CREATE OR REPLACE TABLE taxi-rides-ny.nytaxi.yellow_tripdata_partitoned_clustered
+CREATE OR REPLACE TABLE calcium-petal-376805.ny_taxi_test.yellow_tripdata_partitioned_clustered
 PARTITION BY DATE(tpep_pickup_datetime)
 CLUSTER BY VendorID AS
-SELECT * FROM taxi-rides-ny.nytaxi.external_yellow_tripdata;
+SELECT * FROM calcium-petal-376805.ny_taxi_test.external_yellow_tripdata;
 
 -- Query scans 1.1 GB
 SELECT count(*) as trips
-FROM taxi-rides-ny.nytaxi.yellow_tripdata_partitoned
+FROM calcium-petal-376805.ny_taxi_test.yellow_tripdata_partitioned
 WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2020-12-31'
   AND VendorID=1;
 
 -- Query scans 864.5 MB
 SELECT count(*) as trips
-FROM taxi-rides-ny.nytaxi.yellow_tripdata_partitoned_clustered
+FROM calcium-petal-376805.ny_taxi_test.yellow_tripdata_partitioned_clustered
 WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2020-12-31'
   AND VendorID=1;
 
