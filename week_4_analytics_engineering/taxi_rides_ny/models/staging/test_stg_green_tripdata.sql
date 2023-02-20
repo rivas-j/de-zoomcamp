eff14f9a713b675f3c3a96676271e6e1f17e1133
Jonathan Rivas <jrivas404@gmail.com>
2023-02-19 19:10:49 -0500

    modified test green taxi sql

--- week_4_analytics_engineering/taxi_rides_ny/models/staging/test_stg_green_tripdata.sql
+++ week_4_analytics_engineering/taxi_rides_ny/models/staging/test_stg_green_tripdata.sql
@@ -1,7 +1,6 @@
 {{ config(materialized='view') }}
 
-  select *,
-    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+  select *
   from {{ source('staging','external_green_tripdata') }}
   where vendorid is not null 
   limit 1000
